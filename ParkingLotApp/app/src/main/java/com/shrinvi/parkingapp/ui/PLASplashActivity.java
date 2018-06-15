98f29f552bfb808275100ec25f14841e70c87c86
DineshMasthaiah <dinesh.k.masthaiah@accenture.com>
2018-06-15 19:55:19 +0530

    fixed a compilation issue

--- ParkingLotApp/app/src/main/java/com/shrinvi/parkingapp/ui/PLASplashActivity.java
+++ ParkingLotApp/app/src/main/java/com/shrinvi/parkingapp/ui/PLASplashActivity.java
@@ -3,6 +3,8 @@ package com.shrinvi.parkingapp.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.shrinvi.parkingapp.R;
+
 public class PLASplashActivity extends AppCompatActivity {
 
     @Override
